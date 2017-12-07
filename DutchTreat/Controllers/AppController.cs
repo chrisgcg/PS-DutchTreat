05eb1957e4768f6333367f1679126aaa73fc44da
Christian <chris.phillips80@gmail.com>
2017-12-07 22:36:26 +0000

    Up as far as Grid System in Bootstrap module

---
+++ DutchTreat/Controllers/AppController.cs
@@ -0,0 +1,43 @@
+﻿using DutchTreat.Services;
+using DutchTreat.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DutchTreat.Controllers
+{
+  public class AppController : Controller
+  {
+    private readonly IMailService _mailService;
+
+    public AppController(IMailService mailService)
+    {
+      _mailService = mailService;
+    }
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+    public IActionResult Contact()
+    {
+      ViewBag.Title = "Contact Us";
+      return View();
+    }
+
+    [HttpPost]
+    public IActionResult Contact(ContactViewModel model)
+    {
+      if (ModelState.IsValid) { 
+        _mailService.SendMessage("chris.phillips80@gmail.com", model.Subject, $"From: {model.Email}, Message: {model.Message}");
+        ViewBag.UserMessage = "Mail Sent";
+        ModelState.Clear();
+      }
+      return View();
+    }
+
+    public IActionResult About()
+    {
+      ViewBag.Title = "About Us";
+      return View();
+    }
+  }
+}
